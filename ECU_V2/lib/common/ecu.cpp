c2dbfc5ad7a482f02239a588b10c5dcd94b9ff7d
levi-potter <potterk2720@gmail.com>
2026-02-09 20:29:39 -0700

    Fixed debounce switch

--- ECU_V2/lib/common/ecu.cpp
+++ ECU_V2/lib/common/ecu.cpp
@@ -6,8 +6,10 @@
 #include "can_serde.hpp"
 #include "constants.hpp"
 
-int64_t map(int32_t input_32, int32_t old_min_32, int32_t old_max_32, int32_t new_min_32, int32_t new_max_32) {
-    if (old_min_32 == old_max_32 || new_min_32 == new_max_32) {
+int64_t map(int32_t input_32, int32_t old_min_32, int32_t old_max_32, int32_t new_min_32, int32_t new_max_32)
+{
+    if (old_min_32 == old_max_32 || new_min_32 == new_max_32)
+    {
         /* Avoid division by zero. */
         return 0;
     }
@@ -31,9 +33,10 @@ int64_t map(int32_t input_32, int32_t old_min_32, int32_t old_max_32, int32_t ne
     return output_shifted;
 }
 
-int16_t throttle_map(uint16_t throttle1, uint16_t throttle2) {
+int16_t throttle_map(uint16_t throttle1, uint16_t throttle2)
+{
     /* Make sure the throttle values are in range. */
-    
+
     // FIXME this is throwing an error
     // SAFETY_ASSERT(throttle1 >= THROTTLE1_MIN && throttle1 <= THROTTLE1_MAX);
     // SAFETY_ASSERT(throttle2 >= THROTTLE2_MIN && throttle2 <= THROTTLE2_MAX);
@@ -46,7 +49,7 @@ int16_t throttle_map(uint16_t throttle1, uint16_t throttle2) {
 
     // FIXME this is throwing an error
     /* Make sure the two throttle values haven't diverged too far. */
-    //SAFETY_ASSERT(abs(throttle1_percent - throttle2_percent) < THROTTLE_DISAGREE);
+    // SAFETY_ASSERT(abs(throttle1_percent - throttle2_percent) < THROTTLE_DISAGREE);
 
     int64_t average = (throttle1_percent + throttle2_percent) / 2;
 
@@ -55,53 +58,56 @@ int16_t throttle_map(uint16_t throttle1, uint16_t throttle2) {
     // SAFETY_ASSERT(average >= MIN_THROTTLE && average <= MAX_THROTTLE);
 
     int16_t torque_mapped = map(average, MIN_THROTTLE, MAX_THROTTLE, 0, MAX_TORQUE);
-    // FIXME this isn't working 
+    // FIXME this isn't working
     // SAFETY_ASSERT(torque_mapped >= 0);
-    if (torque_mapped < 0) {
+    if (torque_mapped < 0)
+    {
         torque_mapped = 0;
     }
 
     return torque_mapped;
 }
 
-void Ecu::processMessage(uint32_t current_time_ms, CAN_message_t msg) {
+void Ecu::processMessage(uint32_t current_time_ms, CAN_message_t msg)
+{
     /* If we received a CAN message, update the corresponding value. */
-    switch (static_cast<MessageId>(msg.id)) {
-        case MessageId::StartSwitch:
-            this->start_switch_on = parse_start_switch(msg);
-            break;
-        case MessageId::ThrottleOnePosition:
-            this->throttle1_pos = parse_throttle_one_position(msg);
-            break;
-        case MessageId::ThrottleTwoPosition:
-            this->throttle2_pos = parse_throttle_two_position(msg);
-            break;
-        case MessageId::BrakePressure:
-            this->brake_pressure = parse_brake_pressure(msg);
-            break;
-        default:
-            break;
+    switch (static_cast<MessageId>(msg.id))
+    {
+    case MessageId::StartSwitch:
+        this->start_switch_on = parse_start_switch(msg);
+        break;
+    case MessageId::ThrottleOnePosition:
+        this->throttle1_pos = parse_throttle_one_position(msg);
+        break;
+    case MessageId::ThrottleTwoPosition:
+        this->throttle2_pos = parse_throttle_two_position(msg);
+        break;
+    case MessageId::BrakePressure:
+        this->brake_pressure = parse_brake_pressure(msg);
+        break;
+    default:
+        break;
     }
 
     /* If both throttles have new values, average them and recalculate the throttle amount. */
-    if (this->throttle1_pos.has_value() && this->throttle2_pos.has_value()) {
+    if (this->throttle1_pos.has_value() && this->throttle2_pos.has_value())
+    {
         int16_t mapped_torque = throttle_map(*this->throttle1_pos, *this->throttle2_pos);
         /* After we read the values, invalidate them as they're now stale. */
         this->throttle1_pos = std::nullopt;
         this->throttle2_pos = std::nullopt;
 
         /* mapped_torque must be higher than TORQUE_FLOOR to start the motor. */
-        if (mapped_torque < TORQUE_FLOOR) {
+        if (mapped_torque < TORQUE_FLOOR)
+        {
             mapped_torque = 0;
         }
 
-        // FIXME this isn't working 
+        // FIXME this isn't working
         /* Brake and throttle cannot be pressed at the same time. */
         // SAFETY_ASSERT(!(mapped_torque > 0 && this->brake_pressure >= BRAKE_CONSIDERED_PRESSED));
 
         this->calculated_torque = mapped_torque;
-
-
     }
 
     bool debounced_switch;
@@ -128,6 +134,8 @@ void Ecu::processMessage(uint32_t current_time_ms, CAN_message_t msg) {
         {
             /* If we haven't started the timer yet, go ahead and start it. */
             this->turn_off_timeout.start(current_time_ms, 1000);
+            /* Keep the last value while we wait. */
+            debounced_switch = true;
         }
         else if (this->turn_off_timeout.triggerReached(current_time_ms))
         {
@@ -135,12 +143,20 @@ void Ecu::processMessage(uint32_t current_time_ms, CAN_message_t msg) {
             debounced_switch = false;
             this->last_start_switch_value = false;
         }
+        else
+        {
+            /* Timer is running but hasn't reached yet - keep the old value. */
+            debounced_switch = true;
+        }
     }
 
     /* Car startup sequence. */
-    if (this->car_fully_on) {
+    if (this->car_fully_on)
+    {
         /* No need to do the motor startup sequence if the car is already fully started. */
-    } else {
+    }
+    else
+    {
         /* Startup sequence needed. In order to start up the motor, we need two things:
          *   1. The brake needs to be down.
          *   2. The start switch needs to be on.
@@ -148,10 +164,14 @@ void Ecu::processMessage(uint32_t current_time_ms, CAN_message_t msg) {
          * We will also wait two seconds before fully starting up, or abort if
          * one of the preconditions stops holding. */
         // if ((this->brake_pressure >= BRAKE_CONSIDERED_PRESSED) && this->start_switch_on) {
-        if (debounced_switch) {
-            if (!this->startup_countdown.started()) {
+        if (debounced_switch)
+        {
+            if (!this->startup_countdown.started())
+            {
                 this->startup_countdown.start(current_time_ms, STARTUP_DELAY_MS);
-            } else if (this->startup_countdown.triggerReached(current_time_ms)) {
+            }
+            else if (this->startup_countdown.triggerReached(current_time_ms))
+            {
                 /* Good to go! */
                 this->car_fully_on = true;
             }
@@ -163,28 +183,31 @@ void Ecu::processMessage(uint32_t current_time_ms, CAN_message_t msg) {
         }
     }
 
-    if (!debounced_switch) {
+    if (!debounced_switch)
+    {
         /* Pretty self-explanatory: if the start switch turns off, the car turns off. */
         this->car_fully_on = false;
     }
 }
 
-
-std::optional<CAN_message_t> Ecu::emitMessage(uint32_t current_time_ms) {
+std::optional<CAN_message_t> Ecu::emitMessage(uint32_t current_time_ms)
+{
     bool inverter_enabled = false;
 
     /* Engine is enabled if all the preconditions of `car_fully_on` passed,
      * and if the brake is up. We don't enable the inverter until the
      * driver has lifted up the brake.  */
     // if (this->car_fully_on && this->brake_pressure < BRAKE_CONSIDERED_PRESSED) {
-    if (this->car_fully_on) {
+    if (this->car_fully_on)
+    {
         inverter_enabled = true;
     }
 
     /* The engine needs to be enabled, and the calculated_torque needs to be
      * high enough to actually engage the motor. */
     int16_t torque_to_use = 0;
-    if (inverter_enabled && this->calculated_torque > TORQUE_FLOOR) {
+    if (inverter_enabled && this->calculated_torque > TORQUE_FLOOR)
+    {
         torque_to_use = this->calculated_torque;
     }
 
@@ -193,9 +216,12 @@ std::optional<CAN_message_t> Ecu::emitMessage(uint32_t current_time_ms) {
      * enough time has passed since we started it, `triggerReached` will return true,
      * and mark the trigger as not started. Next time, we'll see that the trigger is
      * marked as not started, and we'll start it again. This loops forever. */
-    if (!this->motor_control_message_pacing.started()) {
-        this->motor_control_message_pacing.start(current_time_ms, 15/*ms*/);
-    } else if (this->motor_control_message_pacing.triggerReached(current_time_ms)) {
+    if (!this->motor_control_message_pacing.started())
+    {
+        this->motor_control_message_pacing.start(current_time_ms, 15 /*ms*/);
+    }
+    else if (this->motor_control_message_pacing.triggerReached(current_time_ms))
+    {
         MotorControlCommand cmd;
         cmd.torque = torque_to_use;
         cmd.speed = 0;
@@ -212,7 +238,8 @@ std::optional<CAN_message_t> Ecu::emitMessage(uint32_t current_time_ms) {
     return std::nullopt;
 }
 
-void Ecu::printState() {
+void Ecu::printState()
+{
     PRINTF("=== ECU State ===\n");
     PRINTF("car_fully_on: %s\n", this->car_fully_on ? "true" : "false");
     PRINTF("start_switch_on: %s\n", this->start_switch_on ? "true" : "false");
@@ -220,15 +247,21 @@ void Ecu::printState() {
     PRINTF("brake_pressure: %u\n", this->brake_pressure);
     PRINTF("calculated_torque: %d\n", this->calculated_torque);
 
-    if (this->throttle1_pos.has_value()) {
+    if (this->throttle1_pos.has_value())
+    {
         PRINTF("throttle1_pos: %u\n", *this->throttle1_pos);
-    } else {
+    }
+    else
+    {
         PRINTF("throttle1_pos: (none)\n");
     }
 
-    if (this->throttle2_pos.has_value()) {
+    if (this->throttle2_pos.has_value())
+    {
         PRINTF("throttle2_pos: %u\n", *this->throttle2_pos);
-    } else {
+    }
+    else
+    {
         PRINTF("throttle2_pos: (none)\n");
     }
 
