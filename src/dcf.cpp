b8ac3f26860d08a7fb8919a9d14748aa7cb871e9
Mason Jones <20848827+smj-edison@users.noreply.github.com>
2026-02-23 10:45:44 -0700

    Part 2: dramatically simplify logic

--- src/dcf.cpp
+++ src/dcf.cpp
@@ -1,45 +1,33 @@
 #include "Arduino.h"
 #include "FlexCAN_T4.h"
 
-#include "DigitalSensor.h"
-#include "AnalogSensor.h"
+#include "util.hpp"
+#include "can_serde.hpp"
 
-constexpr bool THROTTLE_1_CRITICALITY = true;
 constexpr uint8_t THROTTLE_1_PIN = 18;
-
-constexpr bool THROTTLE_2_CRITICALITY = true;
 constexpr uint8_t THROTTLE_2_PIN = 19;
-
-constexpr uint32_t THROTTLE_INTERVAL = 100;
-
-AnalogSensor throttle1 = AnalogSensor(ReservedIDs::Throttle1PositionId, THROTTLE_1_CRITICALITY, THROTTLE_1_PIN, THROTTLE_INTERVAL);
-AnalogSensor throttle2 = AnalogSensor(ReservedIDs::Throttle2PositionId, THROTTLE_1_CRITICALITY, THROTTLE_2_PIN, THROTTLE_INTERVAL);
-
-constexpr bool BRAKE_CRITICALITY = true;
 constexpr uint8_t BRAKE_PIN = 20;
-constexpr uint32_t BRAKE_INTERVAL = 100;
-
-AnalogSensor brake = AnalogSensor(ReservedIDs::BrakePressureId, BRAKE_CRITICALITY, BRAKE_PIN, BRAKE_INTERVAL);
-
-constexpr bool SWITCH_CRITICALITY = true;
 constexpr uint8_t SWITCH_PIN = 21;
-constexpr uint32_t SWITCH_INTERVAL = 100;
 
-DigitalSensor startSwitch = DigitalSensor(ReservedIDs::StartSwitchId, SWITCH_CRITICALITY, SWITCH_PIN, SWITCH_INTERVAL);
-
-Sensor *SENSORS[] = {
-    &throttle1,
-    &throttle2,
-    &brake,
-    &startSwitch,
+/* Results from running sensor_health_check(). */
+struct SensorHealthCheck {
+    bool throttle1_healthy;
+    bool throttle2_healthy;
+    bool brake_healthy;
+    bool switch_health;
 };
-constexpr size_t NUM_SENSORS = sizeof(SENSORS) / sizeof(SENSORS[0]);
-constexpr bool DEBUG = false;
+
 FlexCAN_T4<CAN1, RX_SIZE_256> motorCAN;
 FlexCAN_T4<CAN2, RX_SIZE_256> dataCAN;
 
 constexpr uint32_t CAN_BAUD_RATE = 250000;
 constexpr uint32_t SERIAL_BAUD_RATE = 115200;
+
+/* All these timers control how often messages are emitted for each of these sensors. */
+Timer throttle1_timer(0, 100);
+Timer throttle2_timer(0, 100);
+Timer brake_timer(0, 100);
+Timer start_switch_timer(0, 100);
 
 void setup()
 {
@@ -49,55 +37,76 @@ void setup()
     dataCAN.begin();
     dataCAN.setBaudRate(CAN_BAUD_RATE);
     Serial.println("START");
+
+    pinMode(THROTTLE_1_PIN, INPUT_PULLDOWN);
+    pinMode(THROTTLE_2_PIN, INPUT_PULLDOWN);
+    pinMode(BRAKE_PIN, INPUT_PULLDOWN);
+    /* SWITCH_PIN is an analog input, so we don't need to set its pin mode. */
 }
 
-void sensorHealthCheck() {
-    Health status[NUM_SENSORS];
-    for (size_t sensorIndex = 0; sensorIndex < NUM_SENSORS; sensorIndex++)
-    {
-        status[sensorIndex] = SENSORS[sensorIndex]->healthCheck();
-    }
-    CAN_message_t healthMsg;
-    healthMsg.id = ReservedIDs::DCFId;
-    healthMsg.len = NUM_SENSORS;
-    /* We currently can only report up to 8 statuses, since we only
-     * use a single message. */
-    static_assert(NUM_SENSORS <= 8);
-    for (size_t i = 0; i < NUM_SENSORS; i++)
-    {
-        healthMsg.buf[i] = status[i];
-    }
-    dataCAN.write(healthMsg);
-}
-
-void processSensors() {
-    for (size_t i = 0; i < NUM_SENSORS; i++)
-    {
-        Sensor *sensor = SENSORS[i];
-        if (sensor->ready())
-        {
-            const SensorData data = sensor->read();
-            const CAN_message_t *msgs = data.getMsgs();
-            for (size_t msgIndex = 0; msgIndex < data.getMsgCount(); msgIndex++)
-            {
-                if (sensor->isCritical())
-                {
-                    motorCAN.write(msgs[msgIndex]);
-                }
-                else
-                {
-                    dataCAN.write(msgs[msgIndex]);
-                }
-                if (DEBUG)
-                {
-                    sensor->debugPrint(msgs[msgIndex]);
-                }
-            }
-        }
-    }
-}
+/* We periodically check that all the sensors are working correctly. */
+SensorHealthCheck sensor_health_check();
 
 void loop()
 {
-    processSensors();
+    uint32_t current_time_ms = millis();
+
+    /* Check if any sensors need to send values down the CAN bus, and
+     * if so, send them. */
+    if (throttle1_timer.shouldFire(current_time_ms)) {
+        uint16_t reading = analogRead(THROTTLE_1_PIN);
+        motorCAN.write(create_throttle_one_position(reading));
+    }
+
+    if (throttle2_timer.shouldFire(current_time_ms)) {
+        uint16_t reading = analogRead(THROTTLE_2_PIN);
+        motorCAN.write(create_throttle_two_position(reading));
+    }
+
+    if (brake_timer.shouldFire(current_time_ms)) {
+        uint16_t reading = analogRead(BRAKE_PIN);
+        motorCAN.write(create_brake_pressure(reading));
+    }
+
+    if (start_switch_timer.shouldFire(current_time_ms)) {
+        bool reading = digitalRead(SWITCH_PIN) != 0;
+        motorCAN.write(create_start_switch(reading));
+    }
+}
+
+/* This ensures that the pin is in a low impedence state.
+ * 
+ * Side effects: this will change the pin's pullup/pulldown state,
+ * and will end with the pin in pulldown mode. */
+bool digital_pin_health_check(int pin) {
+    pinMode(pin, INPUT_PULLDOWN);
+
+    /* Impedence check: if we're reading low right now, switch it to
+     * a pullup resistor, and make sure it's still reading low
+     * (if the pin is high impedence, then the pullup resistor
+     * will give a high reading). */
+    if (digitalRead(pin) == LOW) {
+        pinMode(pin, INPUT_PULLUP);
+        if(digitalRead(pin) == LOW) {
+            pinMode(pin, INPUT_PULLDOWN);
+            return true;
+        }
+    }
+
+    /* Make sure we end with the pin in PULLDOWN mode. */
+    pinMode(pin, INPUT_PULLDOWN);
+    return false;
+}
+
+SensorHealthCheck sensor_health_check() {
+    /* By default, everything is considered not healthy, and we
+     * go through each sensor, marking it as healthy if relevant. */
+    SensorHealthCheck result = {false, false, false, false};
+
+    if (analogRead(THROTTLE_1_PIN) != 0) result.throttle1_healthy = true;
+    if (analogRead(THROTTLE_2_PIN) != 0) result.throttle2_healthy = true;
+    if (analogRead(BRAKE_PIN) != 0) result.brake_healthy = true;
+    if (digital_pin_health_check(SWITCH_PIN)) result.switch_health = true;
+
+    return result;
 }
